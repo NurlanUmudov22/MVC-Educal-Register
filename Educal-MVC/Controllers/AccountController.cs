2e7790ba299178d884d32d9a68584b3d270103bd
unknown <nurlanmu@code.edu.az>
2024-06-04 01:00:17 +0400

    add task register

---
+++ Educal-MVC/Controllers/AccountController.cs
@@ -0,0 +1,70 @@
+﻿using Educal_MVC.Models;
+using Educal_MVC.ViewModels.Accounts;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Educal_MVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController( UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+
+        }
+
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterVM request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            AppUser user = new()
+            {
+                FullName = request.FullName,
+                Email = request.Email,
+                UserName = request.UserName,
+            };
+            var result = await _userManager.CreateAsync(user, request.Password);
+
+            if (!result.Succeeded)
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, item.Description);
+                }
+                return View();
+            }
+
+            await _signInManager.SignInAsync(user, false);
+
+
+            return RedirectToAction("Index","Home");
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
